b4ccde9fa07992e32762605e7fd04edb6f47b9ad
bmoran17 <brendamoran1999@gmail.com>
2025-05-12 23:38:53 -0700

    Started home page.

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
 const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.4x9ytlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(express.static(path.join(__dirname + '/public')));
 
+// app.get("/", (req, res) => {
+//     res.render("./views/index.html");
+// });
+app.use((req, res) => {
+    res.status(404);
+    res.send(`<h1>Error 404: Resource Not Found<h1>`);
+})
 async function connect() {
     try {
         await mongoose.connect(uri);
